667cc9f0c52519cf7fc629754bcf09b179012fcf
Arpit Solanki <arpit@corp.miqdigital.com>
2020-01-02 21:57:28 +0530

    changes to app

--- my_app1.py
+++ my_app1.py
@@ -11,6 +11,7 @@ import plotly.graph_objs as go
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 #undervalued=pd.read_csv('undervalued.csv')
 #overvalued=pd.read_csv('overvalued.csv')
